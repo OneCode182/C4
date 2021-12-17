3de3614c813a25fbff712c9949cf9fd2aacd0179
OneCode182 <86318393+OneCode182@users.noreply.github.com>
2021-12-17 09:03:04 -0500

    Add files via upload

---
+++ R2_FULL/src/main/java/usa/ciclo4/reto2/services/CloneService.java
@@ -0,0 +1,194 @@
+/** < - - - - - - - - { G3 - Sergio Silva } - - - - - - - - > */
+
+package usa.ciclo4.reto2.services;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usa.ciclo4.reto2.models.Clone;
+import usa.ciclo4.reto2.repositories.CloneRepo;
+
+
+
+@Service
+public class CloneService {
+    
+    /** < - - - - - - - - { Atributos } - - - - - - - - > */
+    @Autowired
+    private CloneRepo repository;
+    
+    
+    
+    /** < - - - - - - - - { Metodos } - - - - - - - - > */
+    
+    
+    // < CREATE - NEW PRODUCT >
+    public Clone save(Clone product) {
+        if (product.getBrand() == null || product.getProcesor() == null ||
+        product.getOs() == null || product.getDescription() == null ||
+        product.getMemory() == null || product.getHardDrive() == null || 
+        product.getPhotography() == null) {
+            return product;
+            
+        } else {
+            Optional<Clone> productExists = repository.getByID(product.getId());
+            if (productExists.isEmpty()) {
+                return repository.save(product);
+                
+            } return product;
+            
+        }
+        
+    }
+    
+    
+    // < READ - ALL PRODUCTS >
+    public List<Clone> getAll() {
+        return repository.getAll();
+    }
+    
+    // < GET - AMOUNT >
+    public int getAmount() {
+        return repository.getAmount();
+    }
+    
+    // < ALGORITHM - GET MAX ID >
+    private int getMaxID() {
+        if (getAmount() == 0) {
+            return 1;
+            
+        } else {
+            List<Clone> clones = repository.getAll();
+            ArrayList<Integer> ids = new ArrayList<>();
+            
+            for (Clone clone : clones) {
+                ids.add(clone.getId());
+
+            } Collections.sort(ids);
+            
+            return ids.get(ids.size() - 1) + 1;
+            
+        }
+        
+    }
+    
+    // < GET - ASIGN NEW ID >
+    public int getID() {
+        int maxID = getMaxID();
+        if (maxID == 1) {
+            return maxID;
+                   
+        } else {
+            for (int i = 1; i <= maxID; i++) { 
+                Optional<Clone> exist = repository.getByID(i);
+                if (exist.isEmpty()) {
+                    return i;
+
+                }
+
+            } return 0;
+            
+        }
+        
+    }
+    
+    // < READ - BY ID >
+    public Clone getByID(Integer id) {
+        Optional<Clone> exists = repository.getByID(id);
+        if (exists.isPresent()) {
+            return exists.get();
+            
+        } return new Clone();
+    }
+    
+    // <UPDATE>
+    public Clone update(Clone product){
+        if(product.getId() != null){
+            Optional<Clone> productExists = repository.getByID(product.getId());
+            if(productExists.isPresent()){
+                if(product.getBrand() != null){
+                    productExists.get().setBrand(product.getBrand());
+                }
+                if(product.getProcesor() != null){
+                    productExists.get().setProcesor(product.getProcesor());
+                }
+                if(product.getOs() != null){
+                    productExists.get().setOs(product.getOs());
+                }
+                if(product.getDescription() != null){
+                    productExists.get().setDescription(product.getDescription());
+                }
+                if(product.getMemory() != null){
+                    productExists.get().setMemory(product.getMemory());
+                }
+                if(product.getHardDrive() != null){
+                    productExists.get().setHardDrive(product.getHardDrive());
+                }
+                if(true){
+                    productExists.get().setAvailability(product.getAvailability());
+                }
+                if(true){
+                    productExists.get().setPrice(product.getPrice());
+                }
+                if(true){
+                    productExists.get().setQuantity(product.getQuantity());
+                }
+                if(product.getPhotography() != null){
+                    productExists.get().setPhotography(product.getPhotography());
+                }
+                
+                return repository.save(productExists.get());
+                
+            } return product;
+            
+        } return product;
+        
+    }
+    
+    private ArrayList<Integer> getCloneIDS() {
+        List<Clone> clones = repository.getAll();
+        ArrayList<Integer> ids = new ArrayList<>();
+
+        for (Clone clone : clones) {
+            ids.add(clone.getId());
+
+        } return ids;
+        
+    }
+    
+    
+    // < DELETE - USER BY ID>
+    public boolean delete(Integer id) {
+        Optional<Clone> exists = repository.getByID(id);
+        if (exists.isPresent()) {
+            
+            repository.delete(id);
+            
+            return true;
+            
+        } return false;
+        
+    }
+    
+    // < DELETE - ALL >
+    public boolean deleteAll() {
+        try {
+            repository.deleteAll();
+            
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+    
+}
+
+
---
+++ R2_FULL/src/main/java/usa/ciclo4/reto2/services/UserService.java
@@ -0,0 +1,222 @@
+
+/** < - - - - - - - - { G3 - Sergio Silva } - - - - - - - - > */
+
+package usa.ciclo4.reto2.services;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usa.ciclo4.reto2.models.User;
+import usa.ciclo4.reto2.repositories.UserRepo;
+
+
+
+@Service
+public class UserService {
+    
+    /** < - - - - - - - - { Atributos } - - - - - - - - > */
+    @Autowired
+    private UserRepo repository;
+    
+    
+    
+    
+    /** < - - - - - - - - { Metodos } - - - - - - - - > */
+    
+    
+    // < CREATE - NEW USER >
+    public User save(User user) {
+        if (user.getIdentification() == null || user.getName()== null ||
+        user.getMonthBirthtDay() == null || user.getAddress()== null ||
+        user.getCellPhone()== null || user.getEmail()== null ||
+        user.getPassword()== null || user.getZone()== null ||
+        user.getType()== null) {
+            return user;
+            
+        } else {
+            List<User> userExist = repository.getUserByNameOrEmail(user.getName(), user.getEmail());
+            if (userExist.isEmpty()) {
+                if (user.getId() == null) {
+                    return repository.saveUser(user);
+                    
+                } else {
+                    Optional<User> userExist2 = repository.getUserById(user.getId());
+                    if (userExist2.isEmpty()) {
+                        return repository.saveUser(user);
+                        
+                    } else {
+                        return user;
+                        
+                    }
+                    
+                }
+                
+            } else {
+                return user;
+                
+            }
+        }
+        
+    }
+     
+    
+    // < READ - ALL >
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+    
+    // < READ - USER BY ID >
+    public User getUserByID(Integer id) {
+        Optional<User> user = repository.getUserById(id);
+        if (user.isPresent()) {
+            return user.get();
+            
+        } return new User();
+    }
+    
+    // < GET - AMOUNT >
+    public int getAmount() {
+        return repository.getAmount();
+    }
+    
+    // < ALGORITHM - GET MAX ID >
+    private int getMaxID() {
+        if (getAmount() == 0) {
+            return 1;
+            
+        } else {
+            List<User> users = repository.getAll();
+            ArrayList<Integer> ids = new ArrayList<>();
+            
+            for (User user : users) {
+                ids.add(user.getId());
+
+            } Collections.sort(ids);
+            
+            return ids.get(ids.size() - 1) + 1;
+            
+        }
+        
+    }
+    
+    // < GET - ASIGN NEW ID >
+    public int getID() {
+        int maxID = getMaxID();
+        if (maxID == 1) {
+            return maxID;
+                   
+        } else {
+            for (int i = 1; i <= maxID; i++) { 
+                Optional<User> exist = repository.getUserById(i);
+                if (exist.isEmpty()) {
+                    return i;
+
+                }
+
+            } return 0;
+            
+        }
+        
+    }
+    
+    
+    // < EXISTS - EMAIL >
+    
+    public boolean getUserByEmail(String email) {
+        return repository.getUserByEmail(email).isPresent();
+    }
+    
+    // < GET - USER JSON BY EMAIL >
+    
+    public User getUserJsonByEmail(String email) {
+        Optional<User> element = repository.getUserByEmail(email);
+        if (element.isPresent()) {
+            return element.get();
+            
+        } return new User();
+    }
+    
+    
+    
+    
+    // < EXISTS - USER >
+    public boolean getUserByEmailAndPassword(String email, String password) {
+        return repository.getUserByEmailAndPassword(email, password).isPresent();
+        
+    }
+    
+    
+    
+    // < UPDATE >
+    public User update(User user){
+        if(user.getId()!=null){
+            Optional<User> userExist = repository.getUserById(user.getId());
+            if(userExist.isPresent()){
+                if(user.getIdentification()!=null){
+                    userExist.get().setIdentification(user.getIdentification());
+                }
+                if(user.getName()!=null){
+                    userExist.get().setName(user.getName());
+                }
+                if(user.getMonthBirthtDay()!=null){
+                    userExist.get().setMonthBirthtDay(user.getMonthBirthtDay());
+                }
+                if(user.getAddress()!=null){
+                    userExist.get().setAddress(user.getAddress());
+                }
+                if(user.getCellPhone()!=null){
+                    userExist.get().setCellPhone(user.getCellPhone());
+                }
+                if(user.getEmail()!=null){
+                    userExist.get().setEmail(user.getEmail());
+                }
+                if(user.getPassword()!=null){
+                    userExist.get().setPassword(user.getPassword());
+                }
+                if(user.getZone()!=null){
+                    userExist.get().setZone(user.getZone());
+                }
+                if(user.getType()!=null){
+                    userExist.get().setType(user.getType());
+                }
+                return repository.saveUser(userExist.get());
+            }else{
+                return user;
+            }
+        }else{
+            return user;
+        }
+    }
+    
+    
+    // < DELETE - BY ID >
+    public boolean delete(Integer id) {
+        Optional<User> user = repository.getUserById(id);
+        if (user.isPresent()) {
+            repository.deleteById(id);
+            return true;
+            
+        } return false;
+        
+    }
+    
+    // < DELETE - ALL >
+    public boolean deleteAll() {
+        try {
+            repository.deleteAll();
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+    
+}
+
+
