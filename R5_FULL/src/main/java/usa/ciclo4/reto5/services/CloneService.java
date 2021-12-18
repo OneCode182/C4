503397bd9f9a484395147070ffd033cd8c3c916a
OneCode182 <86318393+OneCode182@users.noreply.github.com>
2021-12-17 23:50:37 -0500

    Add files via upload

---
+++ R5_FULL/src/main/java/usa/ciclo4/reto5/services/CloneService.java
@@ -0,0 +1,196 @@
+/** < - - - - - - - - { G3 - Sergio Silva } - - - - - - - - > */
+
+package usa.ciclo4.reto5.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import usa.ciclo4.reto5.models.Clone;
+import usa.ciclo4.reto5.repositories.CloneRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import usa.ciclo4.reto5.models.User;
+
+
+
+@Service
+public class CloneService {
+    
+    /** < - - - - - - - - { Atributos } - - - - - - - - > */
+    @Autowired
+    private CloneRepo repository;
+    
+    
+    
+    /** < - - - - - - - - { Metodos } - - - - - - - - > */
+    
+    
+    // < CREATE - NEW PRODUCT >
+    public Clone save(Clone product) {
+        if (product.getBrand() == null || product.getProcesor() == null ||
+        product.getOs() == null || product.getDescription() == null ||
+        product.getMemory() == null || product.getHardDrive() == null || 
+        product.getPhotography() == null) {
+            return product;
+            
+        } else {
+            product.setId(getID());
+            return repository.save(product);
+            
+        }
+        
+    }
+    
+    // < READ - AMOUNT USERS>
+    public int getAmount() {
+        return repository.getAmount();
+    }
+    
+    // < ALGORITHM - GET MAX ID >
+    private int getMaxID() {
+        if (getAmount() == 0) {
+            return 1;
+            
+        } else {
+            List<Clone> clones = repository.getAll();
+            ArrayList<Integer> ids = new ArrayList<>();
+            
+            for (Clone clone : clones) {
+                ids.add(clone.getId());
+
+            } Collections.sort(ids);
+            
+            return ids.get(ids.size() - 1) + 1;
+            
+        }
+        
+    }
+    
+    // < GET - ASIGN NEW ID >
+    public int getID() {
+        int maxID = getMaxID();
+        if (maxID == 1) {
+            return maxID;
+                   
+        } else {
+            for (int i = 1; i <= maxID; i++) { 
+                Optional<Clone> exist = repository.getByID(i);
+                if (exist.isEmpty()) {
+                    return i;
+
+                }
+
+            } return 0;
+            
+        }
+        
+    }
+    
+    
+    // < READ - ALL PRODUCTS >
+    public List<Clone> getAll() {
+        return repository.getAll();
+    }
+    
+    // < READ - BY ID >
+    public Clone getByID(Integer id) {
+        Optional<Clone> exists = repository.getByID(id);
+        if (exists.isPresent()) {
+            return exists.get();
+            
+        } return new Clone();
+    }
+    
+    // <UPDATE>
+    public Clone update(Clone product){
+        if(product.getId() != null){
+            Optional<Clone> productExists = repository.getByID(product.getId());
+            if(productExists.isPresent()){
+                if(product.getBrand() != null){
+                    productExists.get().setBrand(product.getBrand());
+                }
+                if(product.getProcesor() != null){
+                    productExists.get().setProcesor(product.getProcesor());
+                }
+                if(product.getOs() != null){
+                    productExists.get().setOs(product.getOs());
+                }
+                if(product.getDescription() != null){
+                    productExists.get().setDescription(product.getDescription());
+                }
+                if(product.getMemory() != null){
+                    productExists.get().setMemory(product.getMemory());
+                }
+                if(product.getHardDrive() != null){
+                    productExists.get().setHardDrive(product.getHardDrive());
+                }
+                if(true){
+                    productExists.get().setAvailability(product.getAvailability());
+                }
+                if(true){
+                    productExists.get().setPrice(product.getPrice());
+                }
+                if(true){
+                    productExists.get().setQuantity(product.getQuantity());
+                }
+                if(product.getPhotography() != null){
+                    productExists.get().setPhotography(product.getPhotography());
+                }
+                
+                return repository.save(productExists.get());
+                
+            } return product;
+            
+        } return product;
+        
+    }
+    
+    // < GET - ID EXISTS >
+    public boolean getIdExists(Integer id) {
+        return repository.getByID(id).isPresent();
+    }
+    
+    
+    // < DELETE - BY ID >
+    public boolean deleteById(Integer id) {
+        Optional<Clone> exists = repository.getByID(id);
+        if (exists.isPresent()) {
+            repository.deleteById(id);
+            return true;
+            
+        } return false;
+        
+    }
+    
+    // < DELETE - ALL >
+    public boolean deleteAll() {
+        try {
+            repository.deleteAll();
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+    
+    // < DELETE - BY USER >
+    public boolean deleteByClone(Clone clone) {
+        try {
+            repository.deleteByUser(clone);
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+    
+}
+
+
---
+++ R5_FULL/src/main/java/usa/ciclo4/reto5/services/OrderService.java
@@ -0,0 +1,254 @@
+/** < - - - - - - - - { G3 - Sergio Silva } - - - - - - - - > */
+
+
+package usa.ciclo4.reto5.services;
+
+import usa.ciclo4.reto5.models.Order;
+import usa.ciclo4.reto5.repositories.OrderRepo;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import usa.ciclo4.reto5.models.User;
+
+
+
+
+@Service
+public class OrderService {
+    
+    /** < - - - - - - - - { Atributos } - - - - - - - - > */
+    @Autowired
+    private OrderRepo repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
+
+    /** < - - - - - - - - { Metodos } - - - - - - - - > */
+    
+    
+    // < CREATE - NEW ORDER >
+    public Order save(Order order) {
+        if (order.getRegisterDay() == null || order.getStatus() == null ||
+        order.getSalesMan() == null || order.getProducts() == null ||
+        order.getQuantities() == null) {
+            return order;
+            
+        } else {
+            order.setId(getID());
+            return repository.save(order);
+        }
+        
+    }
+    
+    // < READ - AMOUNT USERS>
+    public int getAmount() {
+        return repository.getAmount();
+    }
+    
+    // < GET - ID EXISTS >
+    public boolean getIdExists(Integer id) {
+        return repository.getById(id).isPresent();
+    }
+    
+    // < ALGORITHM - GET MAX ID >
+    private int getMaxID() {
+        if (getAmount() == 0) {
+            return 1;
+            
+        } else {
+            List<Order> orders = repository.getAll();
+            ArrayList<Integer> ids = new ArrayList<>();
+            
+            for (Order order : orders) {
+                ids.add(order.getId());
+
+            } Collections.sort(ids);
+            
+            return ids.get(ids.size() - 1) + 1;
+            
+        }
+        
+    }
+    
+    // < GET - ASIGN NEW ID >
+    public int getID() {
+        int maxID = getMaxID();
+        if (maxID == 1) {
+            return maxID;
+                   
+        } else {
+            for (int i = 1; i <= maxID; i++) { 
+                Optional<Order> exist = repository.getById(i);
+                if (exist.isEmpty()) {
+                    return i;
+
+                }
+
+            } return 0;
+            
+        }
+        
+    }
+    
+    // < READ - ALL ORDERS >
+    public List<Order> getAll() {
+        return repository.getAll();
+    }
+    
+    // < READ - ORDER BY ID >
+    public Order getByID(Integer id) {
+        Optional<Order> exists = repository.getById(id);
+        if (exists.isPresent()) {
+            return exists.get();
+                    
+        } return new Order();
+    }
+    
+    // < READ - USERS BY ZONE >
+    public List<Order> getUsersByZone(String zone) {
+        return repository.getUserByZone(zone);
+    }
+    
+    // < UPDATE >
+    public Order update(Order order){
+        if(order.getId() != null){
+            Optional<Order> exists = repository.getById(order.getId());
+            if(exists.isPresent()){
+                if(order.getRegisterDay()!= null){
+                    exists.get().setRegisterDay(order.getRegisterDay());
+                }
+                if(order.getStatus()!= null){
+                    exists.get().setStatus(order.getStatus());
+                }
+                if(order.getSalesMan() != null){
+                    exists.get().setSalesMan(order.getSalesMan());
+                }
+                if(order.getProducts()!= null){
+                    exists.get().setProducts(order.getProducts());
+                }
+                if(order.getQuantities()!= null){
+                    exists.get().setQuantities(order.getQuantities());
+                }
+                
+                return repository.save(exists.get());
+                
+            } return order;
+            
+        } return order;
+        
+    }
+    
+    
+
+    // < READ - USERS BY ID >
+    public List<Order> getUsersById(Integer id) {
+        return repository.getByUserId(id);
+    }
+
+    // < READ - USERS BY STATUS >
+    public List<Order> getOrderByStatus(String status) {
+        return repository.getByStatus(status);
+    }
+
+    public List<Order> getByStatusAndByUserId(String state, Integer id) {
+        Query query = new Query();
+        Criteria criterio = Criteria.where("salesMan.id").is(id)
+                .and("status").is(state);
+
+        query.addCriteria(criterio);
+
+        List<Order> orders = mongoTemplate.find(query,Order.class);
+
+        return orders;
+    }
+    
+    // < GET - ORDER BY DATE >
+    public List<Order> getByDate(String date) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Query query = new Query();
+
+        Criteria dateCriteria = Criteria.where("registerDay")
+            .gte(LocalDate.parse(date, dateFormat).minusDays(1).atStartOfDay())
+            .lt(LocalDate.parse(date, dateFormat).plusDays(1).atStartOfDay());
+
+        query.addCriteria(dateCriteria);
+
+        List<Order> orders = mongoTemplate.find(query,Order.class);
+
+        return orders;
+        
+    }
+
+    // < READ - User By ID AND ORDER BY DATE>
+    public List<Order> ordersSalesManByDate(String date, Integer id) {
+        
+        
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Query query = new Query();
+
+        Criteria dateCriteria = Criteria.where("registerDay")
+                .gte(LocalDate.parse(date, dateFormat).minusDays(1).atStartOfDay())
+                .lt(LocalDate.parse(date, dateFormat).plusDays(1).atStartOfDay())
+                .and("salesMan.id").is(id);
+
+        query.addCriteria(dateCriteria);
+
+        List<Order> orders = mongoTemplate.find(query,Order.class);
+
+        return orders;
+        
+    }
+    
+    // < DELETE - BY ID >
+    public boolean deleteById(Integer id) {
+        Optional<Order> exists = repository.getById(id);
+        if (exists.isPresent()) {
+            repository.deleteById(id);
+            return true;
+            
+        } return false;
+        
+    }
+    
+    // < DELETE - ALL >
+    public boolean deleteAll() {
+        try {
+            repository.deleteAll();
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+    
+    // < DELETE - BY ORDER >
+    public boolean deleteByOrder(Order order) {
+        try {
+            repository.deleteByOrder(order);
+            return true;
+            
+        } catch (Exception e) {
+            return false;
+            
+        }
+        
+    }
+
+}
+
